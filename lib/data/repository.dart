f8b2973edf67f1e65d750c413f7e96b2f31b95d5
Carlos75357 <carlosperezalzina@gmail.com>
2024-03-18 12:51:08 +0100

    Login y Search_read funcionan.

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,3 +1,106 @@
 import 'package:flutter_crm_prove/data/repository_data_source.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Repository extends RepositoryDataSource {}
+class Repository extends RepositoryDataSource {
+  String? sessionId;
+  String? url;
+  String? db;
+
+  @override
+  Future<LoginResponse> authenticate(String url, String db, String username, String password) async {
+    var response = await http.post(
+      Uri.parse('$url/web/session/authenticate'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'jsonrpc': '2.0',
+        'params': {
+          'db': 'demos_demos15',
+          'login': username,
+          'password': password
+        }
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      List<String> parts = response.headers['set-cookie']?.split(";") ?? [];
+
+      String? sessionIdPart = parts
+          .firstWhere((part) => part.trim().startsWith("session_id="), orElse: () => "");
+
+      sessionId = sessionIdPart.trim();
+      this.url = url;
+      this.db = db;
+      return LoginResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to authenticate');
+    }
+  }
+
+  @override
+  Future<SearchResponse> searchRead(String? sessionId, String model, List<dynamic> domain) async {
+
+    final response = await http.post(
+      Uri.parse('$url/web/dataset/search_read'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': sessionId ?? '',
+      },
+      body: jsonEncode({
+        'jsonrpc': '2.0',
+        'method': 'call',
+        'params': {
+          'model': model,
+          'domain': domain,
+        },
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseBody = jsonDecode(response.body);
+      List<dynamic> records = responseBody['result']['records'];
+      return SearchResponse.fromJson(records);
+    } else {
+      throw Exception('Failed to perform search_read');
+    }
+  }
+
+
+// @override
+  // Future<ReadResponse> read(String model, List<int> ids) async {
+  //   final url = Uri.parse('https://your-odoo-instance.com/api/$model');
+  //
+  //   final body = jsonEncode({
+  //     'ids': ids,
+  //   });
+  //
+  //   final response = await http.post(
+  //     url,
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Authorization': 'Bearer your-session-token',
+  //     },
+  //     body: body,
+  //   );
+  //
+  //   if (response.statusCode == 200) {
+  //     return ReadResponse.fromJson(jsonDecode(response.body));
+  //   } else {
+  //     throw Exception('Failed to read records');
+  //   }
+  // }
+  //
+  // @override
+  // Future<UnlinkResponse> unlink(String model, List<int> ids) {
+  //   // TODO: implement unlink
+  //   throw UnimplementedError();
+  // }
+  //
+  // @override
+  // Future<WriteResponse> write(String model, List<int> ids, Map<String, dynamic> values) {
+  //   // TODO: implement write
+  //   throw UnimplementedError();
+  // }
+}
--- lib/data/repository_data_source.dart
+++ lib/data/repository_data_source.dart
@@ -1,4 +1,70 @@
 abstract class RepositoryDataSource {
   // TODO DECLARAR AUTHENTICATE, SEARCH_READ, READ, WRITE, UNLINK
-  // Future<LoginResponse> authenticate(String url, String db, String username, String password);
+  Future<LoginResponse> authenticate(String url, String db, String username, String password);
+  Future<SearchResponse> searchRead(String? sessionId, String model, List<dynamic> domain);
+  Future<ReadResponse> read(String model, List<int> ids);
+  // Future<WriteResponse> write(String model, List<int> ids, Map<String, dynamic> values);
+  // Future<UnlinkResponse> unlink(String model, List<int> ids);
+}
+
+class LoginResponse {
+  final int uid;
+  final bool isAdmin;
+  final String db;
+  final String name;
+  final String username;
+
+  LoginResponse({
+    required this.uid,
+    required this.isAdmin,
+    required this.db,
+    required this.name,
+    required this.username,
+  });
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    return LoginResponse(
+      uid: json['result']['uid'],
+      isAdmin: json['result']['is_admin'],
+      db: json['result']['db'],
+      name: json['result']['name'],
+      username: json['result']['username'],
+    );
+  }
+}
+
+class SearchResponse {
+  final List<dynamic> records;
+  SearchResponse(this.records);
+
+  factory SearchResponse.fromJson(List<dynamic> json) {
+    return SearchResponse(json);
+  }
+}
+
+class ReadResponse {
+  final List<dynamic> records;
+  ReadResponse(this.records);
+
+  factory ReadResponse.fromJson(List<dynamic> json) {
+    return ReadResponse(json);
+  }
+}
+
+class WriteResponse {
+  final List<dynamic> records;
+  WriteResponse(this.records);
+
+  factory WriteResponse.fromJson(List<dynamic> json) {
+    return WriteResponse(json);
+  }
+}
+
+class UnlinkResponse {
+  final List<dynamic> records;
+  UnlinkResponse(this.records);
+
+  factory UnlinkResponse.fromJson(List<dynamic> json) {
+    return UnlinkResponse(json);
+  }
 }
