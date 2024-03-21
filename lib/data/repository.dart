86caac05bb3b2e9a6d1dea1e63957cbb7e242916
Carlos75357 <carlosperezalzina@gmail.com>
2024-03-21 12:19:58 +0100

    Modificando Repository y repository data source, método para obtener el nombre de la etiqueta creado, clase CrmLead creada para almacenar los datos requeridos de los lead

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_crm_prove/data/repository_data_source.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+/// Class api to interact with Odoo, based on http package have methods to authenticate, searchRead, read, unlink, write, create
 class Repository extends RepositoryDataSource {
   String? sessionId;
   String? url;
   String? db;
 
+  /// Authenticate method, send credentials to login in Odoo, if credentials
+  /// are correct saves sessionId in sessionId variable to be used in other methods.
   @override
   Future<LoginResponse> authenticate(String url, String db, String username, String password) async {
     var response = await http.post(
@@ -39,9 +41,10 @@ class Repository extends RepositoryDataSource {
     }
   }
 
+  /// SearchRead method, send domain to search in Odoo all records that match
+  /// with the domain
   @override
-  Future<SearchResponse> searchRead(String model, List<dynamic> domain) async {
-
+  Future<List<CrmLead>> searchRead(String model, List<dynamic> domain) async {
     final response = await http.post(
       Uri.parse('$url/web/dataset/search_read'),
       headers: {
@@ -61,15 +64,21 @@ class Repository extends RepositoryDataSource {
     if (response.statusCode == 200) {
       Map<String, dynamic> responseBody = jsonDecode(response.body);
       List<dynamic> records = responseBody['result']['records'];
-      return SearchResponse.fromJson(records);
+      if (records.isNotEmpty) {
+        return records.map((record) => CrmLead.fromJson(record)).toList();
+      } else {
+        throw Exception('No records found');
+      }
     } else {
-      throw Exception('Failed to perform search_read');
+      throw Exception('Failed to perform search_read with status code ${response.statusCode}');
     }
   }
 
-
+  /// Read method, read the records with the given ids from the given model,
+  /// in args must be the id and in kwargs the fields that you want to read
+  /// in this case is empty, which means that it will read all the fields
   @override
-  Future<ReadResponse> read(String model, List<int> id) async {
+  Future<CrmLead> read(String model, int id) async {
     final response = await http.post(
       Uri.parse('$url/web/dataset/call_kw/crm/read'),
       headers: {
@@ -89,24 +98,22 @@ class Repository extends RepositoryDataSource {
     );
 
     if (response.statusCode == 200) {
-      print(response.body);
       Map<String, dynamic> responseBody = jsonDecode(response.body);
-      dynamic result = responseBody['result'];
-
-      if (result != null) {
-        return ReadResponse.fromJson(result);
-      } else {
-        throw Exception('No records found in the response');
+      List<dynamic> records = responseBody['result'];
+      for (var record in records) {
+        if (record['id'] == id) {
+          return CrmLead.fromJson(record);
+        }
       }
+      throw Exception('No record found with id $id');
     } else {
-      throw Exception('Failed to read records');
+      throw Exception('Failed to read record with status code ${response.statusCode}');
     }
   }
 
-  /// Unlink the records with the given ids from the given model
-  ///
+  /// Unlink the record with the given id from the given model
   @override
-  Future<UnlinkResponse> unlink(String model, List<int> ids) async {
+  Future<UnlinkResponse> unlink(String model, int id) async {
     final response = await http.post(
       Uri.parse('$url/web/dataset/call_kw/crm/unlink'),
       headers: {
@@ -119,7 +126,7 @@ class Repository extends RepositoryDataSource {
         'params': {
           'model': model,
           'method': 'unlink',
-          'args': [ids],
+          'args': [[id]],
           'kwargs': {'context': []},
         }
       }),
@@ -139,8 +146,9 @@ class Repository extends RepositoryDataSource {
     }
   }
 
+  /// Write method, update the record with the given id.
   @override
-  Future<WriteResponse> write(String model, List<int> ids, Map<String, dynamic> values) async {
+  Future<WriteResponse> write(String model, int id, CrmLead values) async {
     final response = await http.post(
       Uri.parse('$url/web/dataset/call_kw/crm/write'),
       headers: {
@@ -153,7 +161,7 @@ class Repository extends RepositoryDataSource {
         'params': {
           'model': model,
           'method': 'write',
-          'args': [ids, values],
+          'args': [id, values.toJson()],
           'kwargs': {'context': []},
         }
       }),
@@ -173,6 +181,7 @@ class Repository extends RepositoryDataSource {
     }
   }
 
+  /// Create method, create a new record in the given model with the given values
   @override
   Future<CreateResponse> create(String model, Map<String, dynamic> values) async {
     final response = await http.post(
@@ -199,5 +208,39 @@ class Repository extends RepositoryDataSource {
     } else {
       throw Exception('Failed to create record');
     }
+  }
+
+  Future<List<String>> tagNames(List<dynamic>? tagIds) async {
+    List<String> tagNames = [];
+    if (tagIds != null && tagIds.isNotEmpty) {
+      for (var tagId in tagIds) {
+        final response = await http.post(
+          Uri.parse('$url/web/dataset/call_kw/tags/read'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Cookie': sessionId ?? '',
+          },
+          body: jsonEncode({
+            'jsonrpc': '2.0',
+            'method': 'call',
+            'params': {
+              'model': 'crm.tag',
+              'method': 'read',
+              'args': [tagId],
+              'kwargs': {'fields': ['name'], 'context': []},
+            }
+          }),
+        );
+
+        if (response.statusCode == 200) {
+          Map<String, dynamic> responseBody = jsonDecode(response.body);
+          String tagName = responseBody['result'][0]['name'];
+          tagNames.add(tagName);
+        } else {
+          throw Exception('Failed to read tag with status code ${response.statusCode}');
+        }
+      }
+    }
+    return tagNames;
   }
 }
--- lib/data/repository_data_source.dart
+++ lib/data/repository_data_source.dart
@@ -1,57 +1,125 @@
 abstract class RepositoryDataSource {
   // TODO DECLARAR AUTHENTICATE, SEARCH_READ, READ, WRITE, UNLINK
   Future<LoginResponse> authenticate(String url, String db, String username, String password);
-  Future<SearchResponse> searchRead(String model, List<dynamic> domain);
-  Future<ReadResponse> read(String model, List<int> id);
-  Future<UnlinkResponse> unlink(String model, List<int> ids);
-  Future<WriteResponse> write(String model, List<int> ids, Map<String, dynamic> values);
+  Future<List<CrmLead>> searchRead(String model, List<dynamic> domain);
+  Future<CrmLead> read(String model, int id);
+  Future<UnlinkResponse> unlink(String model, int id);
+  Future<WriteResponse> write(String model, int id, CrmLead values);
   Future<CreateResponse> create(String model, Map<String, dynamic> values);
 }
 
+/// Class LoginResponse to handle response from authenticate, if there is an
+/// error, the bool success will be false.
 class LoginResponse {
-  final int uid;
-  final bool isAdmin;
-  final String db;
-  final String name;
-  final String username;
+  final bool success;
+  final String? errorMessage;
 
   LoginResponse({
-    required this.uid,
-    required this.isAdmin,
-    required this.db,
-    required this.name,
-    required this.username,
+    required this.success,
+    this.errorMessage,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
-    return LoginResponse(
-      uid: json['result']['uid'],
-      isAdmin: json['result']['is_admin'],
-      db: json['result']['db'],
-      name: json['result']['name'],
-      username: json['result']['username'],
+    if (json.containsKey('error')) {
+      return LoginResponse(
+        success: false,
+        errorMessage: json['error']['message'],
+      );
+    } else {
+      return LoginResponse(
+        success: true,
+      );
+    }
+  }
+}
+
+class CrmLead {
+  final int id;
+  final String? name;
+  final String? description;
+  final String? phone;
+  final String? email;
+  final String? company;
+  final String? commercial;
+  final String? date_limit;
+  final String? sales_team;
+  final String? expected_income;
+  final List<int>? tagIds;
+  final String? priority;
+  final String? probability;
+  final String? date_create;
+  final String? stage;
+
+  CrmLead({
+    required this.id,
+    required this.name,
+    this.description,
+    this.phone,
+    this.email,
+    this.company,
+    this.commercial,
+    this.date_limit,
+    this.sales_team,
+    this.expected_income,
+    this.tagIds,
+    this.priority,
+    this.probability,
+    this.date_create,
+    this.stage,
+  });
+
+  factory CrmLead.fromJson(Map<String, dynamic> json) {
+    final phone = json['phone'] is bool && !json['phone'] ? null : json['phone'];
+    final List<dynamic>? tagsJson = json['tag_ids'];
+    List<int>? tagIds;
+    if (tagsJson != null) {
+      tagIds = tagsJson.map<int>((dynamic id) => id as int).toList();
+    }
+    return CrmLead(
+      id: json['id'],
+      name: json['name'],
+      phone: phone,
+      email: json['email'],
+      company: json['company'],
+      commercial: json['commercial'],
+      date_limit: json['date_limit'],
+      sales_team: json['sales_team'],
+      expected_income: json['expected_income'],
+      tagIds: tagIds,
+      priority: json['priority'],
+      probability: double.parse(json['probability'].toString()).toStringAsFixed(2),
+      date_create: json['date_create'],
+      stage: json['stage'],
     );
   }
-}
 
-class SearchResponse {
-  final List<dynamic> records;
-  SearchResponse(this.records);
-
-  factory SearchResponse.fromJson(List<dynamic> json) {
-    return SearchResponse(json);
+  @override
+  String toString() {
+    return 'Id: $id - Nombre: $name - Email: $email - Teléfono: $phone - Compañia: $company - Fecha límite: $date_limit - Sales Team: $sales_team - Expected Income: $expected_income - Tags: $tagIds - Priority: $priority - Probability: $probability - Date Create: $date_create - Stage: $stage';
   }
-}
 
-class ReadResponse {
-  final List<dynamic> records;
-  ReadResponse(this.records);
-
-  factory ReadResponse.fromJson(List<dynamic> json) {
-    return ReadResponse(json);
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    if (name != null) data['name'] = name;
+    if (phone != null) data['phone'] = phone;
+    if (email != null) data['email'] = email;
+    if (company != null) data['company'] = company;
+    if (commercial != null) data['commercial'] = commercial;
+    if (date_limit != null) data['date_limit'] = date_limit;
+    if (sales_team != null) data['sales_team'] = sales_team;
+    if (expected_income != null) data['expected_income'] = expected_income;
+    if (tagIds != null) data['tag_ids'] = tagIds;
+    if (priority != null) data['priority'] = priority;
+    if (probability != null) data['probability'] = probability;
+    if (date_create != null) data['date_create'] = date_create;
+    if (stage != null) data['stage'] = stage;
+    return data;
   }
+
 }
 
+/// Class UnlinkResponse to handle response from unlink
 class UnlinkResponse {
   final List<dynamic> records;
   UnlinkResponse(this.records);
@@ -61,6 +129,8 @@ class UnlinkResponse {
   }
 }
 
+/// Class WriteResponse to handle response from write, if there is an error,
+/// the bool success will be false and the int id will be null.
 class WriteResponse {
   final bool success;
   final bool? id;
@@ -68,25 +138,39 @@ class WriteResponse {
   WriteResponse({required this.success, this.id});
 
   factory WriteResponse.fromJson(Map<String, dynamic> json) {
-    return WriteResponse(
-      success: true,
-      id: json['result'],
-    );
+    if (json.containsKey('error')) {
+      return WriteResponse(
+        success: false,
+        id: null,
+      );
+    } else {
+      return WriteResponse(
+        success: true,
+        id: json['result'],
+      );
+    }
   }
 }
 
-
+/// Class CreateResponse to handle response from create, if there is an error,
+/// the bool success will be false and the int id will be null.
 class CreateResponse {
   final bool success;
-  final int id;
+  final int? id;
 
   CreateResponse({required this.success, required this.id});
 
   factory CreateResponse.fromJson(Map<String, dynamic> json) {
-    int id = json['result'];
-    return CreateResponse(
-      success: id > 0,
-      id: id,
-    );
+    if (json.containsKey('error')) {
+      return CreateResponse(
+        success: false,
+        id: null,
+      );
+    } else {
+      return CreateResponse(
+        success: true,
+        id: json['result'],
+      );
+    }
   }
 }
