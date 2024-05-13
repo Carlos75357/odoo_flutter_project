2a2d4e172dc56574bb40a6b09a2e41c295241892
Carlos75357 <carlosperezalzina@gmail.com>
2024-05-13 13:11:12 +0200

    Ahora los colores del modo claro/oscuro los recoge del remote config

--- lib/remote_config_service.dart
+++ lib/remote_config_service.dart
@@ -1,4 +1,7 @@
 
+
+import 'dart:ui';
+
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 class RemoteConfigService {
@@ -40,10 +43,32 @@ class RemoteConfigService {
   }
 
   void handleConfigUpdates() {
-  // TODO: Implementar la lógica para manejar las actualizaciones de configuración
+
   }
+
+  Color rgbToColor(String code) {
+    List<int> rgb = code.split(',').map((String value) => int.parse(value.trim())).toList();
+    for (int i = 0; i < rgb.length; i++) {
+      print(rgb[i]);
+    }
+    return Color.fromRGBO(rgb[0], rgb[1], rgb[2], 1.0);
+  }
+
 
   bool get canCreateCrmLeads => remoteConfig.getBool('can_create_crm_leads');
   bool get canDeleteCrmLeads => remoteConfig.getBool('can_delete_crm_leads');
   int get crmLeadsLimit => remoteConfig.getInt('crm_leads_limit');
+  String get primaryLight => remoteConfig.getString('primary_light');
+  String get primaryDark => remoteConfig.getString('primary_dark');
+  String get backgroundLight => remoteConfig.getString('background_light');
+  String get backgroundDark => remoteConfig.getString('background_dark');
+  String get floatingButtonLight => remoteConfig.getString('floating_button_light');
+  String get floatingButtonDark => remoteConfig.getString('floating_button_dark');
+
+  Color get primaryLightColor => rgbToColor(primaryLight);
+  Color get primaryDarkColor => rgbToColor(primaryDark);
+  Color get backgroundLightColor => rgbToColor(backgroundLight);
+  Color get backgroundDarkColor => rgbToColor(backgroundDark);
+  Color get floatingButtonLightColor => rgbToColor(floatingButtonLight);
+  Color get floatingButtonDarkColor => rgbToColor(floatingButtonDark);
 }
